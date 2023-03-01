0e2525a6c66a6f10af5a158592ef3d75d28633c6
churkakiller2012 <103064791+churkakiller2012@users.noreply.github.com>
2023-03-01 18:12:43 +0300

    rebeauty commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.getElementById('last-link').addEventListener('click', scrollToElement());
+// function scrollToElement(div) {
+//     element = document.getElementById('last')
+//     element.scrollInteView(true);
+// }
+
+const jQuery = require("./jQuery");
+
+// inView('.wrapper').once('enter', runAnimation);
+
+
